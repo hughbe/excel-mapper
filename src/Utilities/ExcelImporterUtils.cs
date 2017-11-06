936b154599a5662cfa7b540789abb0b161b49efc
Marcos <juninhowblu@gmail.com>
2017-11-06 11:47:15 -0200

    Added RegisterMapperClassesByNamespace

---
+++ src/Utilities/ExcelImporterUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ExcelMapper.Utilities
+{
+    public static class ExcelImporterUtils
+    {
+        public static List<ExcelClassMap> RegisterMapperClassesByNamespace(this ExcelImporter importer, string namespacestr)
+        {
+            var classes = Assembly.GetExecutingAssembly()
+                .GetTypes().AsParallel()
+                .Where(t => t.IsClass && t.Namespace == namespacestr);
+
+            var objects = classes.AsParallel().Select(Activator.CreateInstance)
+                .AsParallel().OfType<ExcelClassMap>().ToList();
+
+            foreach (var o in objects)
+                importer.Configuration.RegisterClassMap(o);
+
+            return objects;
+        }
+    }
+}
