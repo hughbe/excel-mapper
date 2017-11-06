1e49949d55c87b59278ff3ada05e53c358af0bdd
Marcos <juninhowblu@gmail.com>
2017-11-06 12:12:16 -0200

    Moved to ExcelMapper namespace, but kept it on utils foder, for organization purposes

--- src/Utilities/ExcelImporterUtils.cs
+++ src/Utilities/ExcelImporterUtils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace ExcelMapper.Utilities
+namespace ExcelMapper
 {
     public static class ExcelImporterUtils
     {
