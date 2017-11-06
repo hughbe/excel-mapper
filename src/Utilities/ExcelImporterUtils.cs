4bdaaf245a34ca3c101931b810138853a58a2109
Marcos <juninhowblu@gmail.com>
2017-11-06 12:22:51 -0200

    Minor changes

--- src/Utilities/ExcelImporterUtils.cs
+++ src/Utilities/ExcelImporterUtils.cs
@@ -8,15 +8,24 @@ namespace ExcelMapper
     public static class ExcelImporterUtils
     {
 #if NETSTANDARD2_0
-        public static List<ExcelClassMap> RegisterMapperClassesByNamespace(this ExcelImporter importer,
-            string namespacestr)
+        public static IEnumerable<ExcelClassMap> RegisterMapperClassesByNamespace(this ExcelImporter importer,
+            string nameSpace)
         {
+            if (nameSpace == null)
+            {
+                throw new ArgumentNullException(nameof(nameSpace));
+            }
+
+            if (nameSpace.Length == 0)
+            {
+                throw new ArgumentException("The namespace cannot be empty.", nameof(nameSpace));
+            }
+
             var classes = Assembly.GetExecutingAssembly()
                 .GetTypes().AsParallel()
-                .Where(t => t.IsClass && t.Namespace == namespacestr);
+                .Where(t => t.IsAssignableFrom(typeof(ExcelClassMap)) && t.Namespace == nameSpace);
 
-            var objects = classes.AsParallel().Select(Activator.CreateInstance)
-                .AsParallel().OfType<ExcelClassMap>().ToList();
+            var objects = classes.AsParallel().Select(Activator.CreateInstance).OfType<ExcelClassMap>().ToList();
 
             foreach (var o in objects)
                 importer.Configuration.RegisterClassMap(o);
