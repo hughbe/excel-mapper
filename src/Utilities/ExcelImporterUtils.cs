5dc9432360d3731afc14e2e28f9bee23430702cf
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-11-07 09:23:09 -0200

    Auto register mapper classes by namespace (#6)
    * Moved to NetStandard 2.0
    
    * Added RegisterMapperClassesByNamespace
    
    * Trying to fix tests
    
    * Made new util work only on netstandard2.0
    
    * Made test compatible with netcoreapp1.1
    
    * Moved to ExcelMapper namespace, but kept it on utils foder, for organization purposes
    
    * Minor changes
    
    * Included System.Reflection to support on netstandard1.4
    
    * renamed nameSpace to namespaceString
    
    * Remvoed netcoreapp2 from tests
    
    * Reverted tests project packages to original version
    
    * Update test project packages
    
    * Added more parameter validation
    
    * Removed unnecessary braces

---
+++ src/Utilities/ExcelImporterUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ExcelMapper
+{
+    public static class ExcelImporterUtils
+    {
+#if NETSTANDARD2_0
+        public static IEnumerable<ExcelClassMap> RegisterClassMapsInNamespace(this ExcelImporter importer,
+            string namespaceString)
+        {
+            return RegisterClassMapsInNamespace(importer, Assembly.GetExecutingAssembly(), namespaceString);
+        }
+#endif
+
+        public static IEnumerable<ExcelClassMap> RegisterClassMapsInNamespace(this ExcelImporter importer,
+            Assembly assembly,
+            string namespaceString)
+        {
+            if (namespaceString == null)
+                throw new ArgumentNullException(nameof(namespaceString));
+
+            if (namespaceString.Length == 0)
+                throw new ArgumentException("The namespace cannot be empty.", nameof(namespaceString));
+
+            if (!assembly.GetTypes().Any())
+                throw new ArgumentException("The assembly doesn't have any types.", nameof(assembly));
+
+            var classes = assembly
+                .GetTypes()
+                .Where(t => t.IsAssignableFrom(typeof(ExcelClassMap)) && t.Namespace == namespaceString);
+
+            var objects = classes.Select(Activator.CreateInstance).OfType<ExcelClassMap>().ToList();
+
+            if (!objects.Any())
+                throw new ArgumentException("No classmaps found in this namespace.", nameof(assembly));
+
+            foreach (var o in objects)
+                importer.Configuration.RegisterClassMap(o);
+
+            return objects;
+        }
+    }
+}
