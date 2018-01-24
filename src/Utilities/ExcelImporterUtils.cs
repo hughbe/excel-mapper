d6a18e4c954ea08548a932c5808c6d1cd0d6a396
Hugh Bellamy <hughbellars@gmail.com>
2018-01-24 17:15:01 +0100

    Add ExcelImporterUtils tests and fix bugs (#14)

--- src/Utilities/ExcelImporterUtils.cs
+++ src/Utilities/ExcelImporterUtils.cs
@@ -8,39 +8,45 @@ namespace ExcelMapper
     public static class ExcelImporterUtils
     {
 #if NETSTANDARD2_0
-        public static IEnumerable<ExcelClassMap> RegisterClassMapsInNamespace(this ExcelImporter importer,
-            string namespaceString)
+        public static IEnumerable<ExcelClassMap> RegisterClassMapsInNamespace(this ExcelImporter importer, string namespaceString)
         {
             return RegisterClassMapsInNamespace(importer, Assembly.GetExecutingAssembly(), namespaceString);
         }
 #endif
 
-        public static IEnumerable<ExcelClassMap> RegisterClassMapsInNamespace(this ExcelImporter importer,
-            Assembly assembly,
-            string namespaceString)
+        public static IEnumerable<ExcelClassMap> RegisterClassMapsInNamespace(this ExcelImporter importer, Assembly assembly, string namespaceString)
         {
+            if (assembly == null)
+            {
+                throw new ArgumentNullException(nameof(assembly));
+            }
+
             if (namespaceString == null)
+            {
                 throw new ArgumentNullException(nameof(namespaceString));
+            }
 
             if (namespaceString.Length == 0)
+            {
                 throw new ArgumentException("The namespace cannot be empty.", nameof(namespaceString));
+            }
 
-            if (!assembly.GetTypes().Any())
-                throw new ArgumentException("The assembly doesn't have any types.", nameof(assembly));
-
-            var classes = assembly
+            IEnumerable<Type> classMapTypes = assembly
                 .GetTypes()
-                .Where(t => t.IsAssignableFrom(typeof(ExcelClassMap)) && t.Namespace == namespaceString);
+                .Where(type => typeof(ExcelClassMap).IsAssignableFrom(type) && type.Namespace == namespaceString);
 
-            var objects = classes.Select(Activator.CreateInstance).OfType<ExcelClassMap>().ToList();
+            ExcelClassMap[] classMaps = classMapTypes.Select(Activator.CreateInstance).OfType<ExcelClassMap>().ToArray();
+            if (classMaps.Length == 0)
+            {
+                throw new ArgumentException($"No classmaps found in the namespace \"{namespaceString}\" in the assembly \"{assembly}\".", nameof(namespaceString));
+            }
 
-            if (!objects.Any())
-                throw new ArgumentException("No classmaps found in this namespace.", nameof(assembly));
+            foreach (ExcelClassMap classMap in classMaps)
+            {
+                importer.Configuration.RegisterClassMap(classMap);
+            }
 
-            foreach (var o in objects)
-                importer.Configuration.RegisterClassMap(o);
-
-            return objects;
+            return classMaps;
         }
     }
 }
