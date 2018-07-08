d443e5f672ac8b91381ab429906968b9f580a264
Hugh Bellamy <hughbellars@gmail.com>
2018-07-08 16:30:52 +0100

    Cleanup coverage

--- coverage.bat
+++ coverage.bat
@@ -1,11 +1,3 @@
-@echo off
-
-dotnet test tests -f netcoreapp2.1 /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-
-set report_generator_version=3.1.2
-set user_directory=C:\Users\hughb
-set packages_directory=%user_directory%\.nuget\packages
-set report_generator=%packages_directory%\reportgenerator\%report_generator_version%\tools\ReportGenerator.exe
-
-set report_path=resources\coverage
-%report_generator% -reports:"tests\coverage.opencover.xml" -targetdir:"%report_path%"
+cd tests
+dotnet test -f netcoreapp2.1 /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet reportgenerator -reports:coverage.opencover.xml -targetdir:../resources/coverage
